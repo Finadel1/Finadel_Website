31b4c10ce7aa1173ae663337a53f02343df43923
Devanshu Saxena <86067292+devanshu0605@users.noreply.github.com>
2022-08-26 08:55:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -115,13 +115,65 @@ a.controller("register", function ($scope, $http, $location) {
 a.controller("login", function ($scope, $http, $location) {
   $scope.pass = ""
   $scope.user = ""
+  // $scope.func = function () {
+
+  //   var pass = $scope.pass;
+
+  //   var user = $scope.user;
+
+  //   var obj = { pass: pass, user: user }
+
+  //   if (pass == "" || user == "") {
+  //     swal.fire({
+  //       icon: 'error',
+  //       title: 'Oops...',
+  //       text: "Data Missing "
+  //     })
+  //     return;
+  //   }
+  //   console.log(obj);
+  //   $http({
+  //     method: "POST",
+  //     url: window.baseUrl + "/user/login/",
+  //     data: JSON.stringify(obj),
+  //     withCredentials: true,
+
+  //   })
+
+  //     .then(function (response) {
+  //       console.log(response.status);
+  //       {
+
+  //         Swal.fire(
+  //           'Good job!',
+  //           'You are now logged in ',
+  //           'success'
+  //         )
+
+  //         $location.path("/user.html");
+
+  //       }
+
+
+  //     })
+  //     .catch((response) => {
+
+  //       swal.fire({
+  //         icon: 'error',
+  //         title: 'Oops...',
+  //         text: response.data.error
+  //       })
+
+  //       console.log(err)
+  //     })
+  // }
   $scope.func = function () {
 
     var pass = $scope.pass;
 
     var user = $scope.user;
 
-    var obj = { pass: pass, user: user }
+
 
     if (pass == "" || user == "") {
       swal.fire({
@@ -129,44 +181,56 @@ a.controller("login", function ($scope, $http, $location) {
         title: 'Oops...',
         text: "Data Missing "
       })
+
+    }
+    $scope.p = "devanshu";
+    $scope.e = "user1";
+    if ($scope.pass == $scope.p && $scope.user == $scope.e) {
+      Swal.fire(
+        'Good job!',
+        'You are now logged in ',
+        'success'
+      )
+      $location.url("/user.html");
       return;
     }
-    console.log(obj);
-    $http({
-      method: "POST",
-      url: window.baseUrl + "/user/login/",
-      data: JSON.stringify(obj),
-      withCredentials: true,
-
-    })
-
-      .then(function (response) {
-        console.log(response.status);
-        {
-
-          Swal.fire(
-            'Good job!',
-            'You are now logged in ',
-            'success'
-          )
-
-          $location.path("/user.html");
-
-        }
-
-
+    else {
+      swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'You entered wrong email or password',
+        footer: '<a href="">Forgot Password?</a>'
       })
-      .catch((response) => {
+    }
 
-        swal.fire({
-          icon: 'error',
-          title: 'Oops...',
-          text: response.data.error
-        })
 
-        console.log(err)
+
+    $scope.p1 = "devanshu";
+    $scope.e1 = "shop1";
+    if ($scope.pass == $scope.p1 && $scope.user == $scope.e1) {
+      Swal.fire(
+        'Good job!',
+        'You are now logged in ',
+        'success'
+      )
+      $location.url("/vendordashboard.html");
+      return;
+    }
+    else {
+      swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'You entered wrong email or password',
+        footer: '<a href="">Forgot Password?</a>'
       })
+    }
+
+
+
+
   }
+
+  
 })
 a.controller("user", function ($scope, $http, $location) {
 
@@ -224,3 +288,4 @@ a.controller("adminlogin", function ($scope, $location) {
   }
 
 })
+
